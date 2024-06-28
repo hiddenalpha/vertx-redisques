fa37e98a42a4396b2e60bfeb55c9a4377bde63d8
Andreas Fankhauser <23085769+hiddenalpha@users.noreply.github.com>
2024-06-28 18:00:29 +0200

    [SDCISA-16293] Continue full re-impl. Not much impl yet.

--- src/main/java/org/swisspush/redisques/util/QueueStatisticsCollector.java
+++ src/main/java/org/swisspush/redisques/util/QueueStatisticsCollector.java
@@ -1,5 +1,8 @@
 package org.swisspush.redisques.util;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonBooleanFormatVisitor;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
@@ -10,6 +13,11 @@ import io.vertx.core.json.JsonObject;
 import org.slf4j.Logger;
 import org.swisspush.redisques.exception.RedisQuesExceptionFactory;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UncheckedIOException;
+import java.io.Writer;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -20,10 +28,15 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.BiConsumer;
 
+import static java.lang.Math.max;
 import static java.lang.System.currentTimeMillis;
 import static org.slf4j.LoggerFactory.getLogger;
 
+import static io.vertx.core.Future.succeededFuture;
 import static java.lang.Thread.currentThread;
+import static org.swisspush.redisques.util.RedisquesAPI.MONITOR_QUEUE_NAME;
+import static org.swisspush.redisques.util.RedisquesAPI.QUEUES;
+import static org.swisspush.redisques.util.RedisquesAPI.STATUS;
 
 /**
  * Class StatisticsCollector helps collecting statistics information about queue handling and
@@ -42,38 +55,61 @@ import static java.lang.Thread.currentThread;
  */
 public class QueueStatisticsCollector {
 
+    private static final String queueUpdatesAddr = "redisques." + QueueStatisticsCollector.class.getSimpleName() + ".jAQCAO0YAgDrSQIA";
+    private final long publishEveryMs = 10_000; /*TODO ctor-inject*/
     private static final Logger log = getLogger(QueueStatisticsCollector.class);
+    private final Vertx vertx;
     private final EventBus evBus;
+    private final ObjectMapper objectMapper = new ObjectMapper();
     private final Lock mutx = new ReentrantLock();
     private Thread mutxOwner;
-    private final Map<String, Queue> queues = new HashMap<>();
-    private final Set<Queue> hasLocalChanges = new HashSet<>();
+    private final Map<String, QueueInternal> queues = new HashMap<>();
+    private Set<QueueInternal> hasLocalChanges = new HashSet<>();
 
     public QueueStatisticsCollector(
-        RedisProvider redisProvider, String queuesPrefix, Vertx vertx, RedisQuesExceptionFactory exceptionFactory,
-        Semaphore redisMonitoringReqQuota, int queueSpeedIntervalSec
+            RedisProvider redisProvider, String queuesPrefix, Vertx vertx, RedisQuesExceptionFactory exceptionFactory,
+            Semaphore redisMonitoringReqQuota, int queueSpeedIntervalSec
     ) {
-        long publishEveryMs = 1000;
-        String evBusAddr = QueueStatisticsCollector.class.getCanonicalName();
-        evBus = vertx.eventBus();
-        evBus.consumer(evBusAddr, this::onUpdateFromClusterPeer);
-        vertx.setTimer(publishEveryMs, this::publishLocalChanges);
-    }
-
-    private void onUpdateFromClusterPeer(Message<Object> evBusMsg) {
-        Object body = evBusMsg.body();
-        assert false : "TODO";
+        this.vertx = vertx;
+        this.evBus = vertx.eventBus();
+        this.evBus.consumer(queueUpdatesAddr, this::onUpdateFromCluster);
+        this.vertx.setTimer(publishEveryMs, this::publishLocalChanges);
     }
 
     private void publishLocalChanges(long nonsense) {
-        Set<Queue> localCopy;
+        Set<QueueInternal> copyToPublish;
+        // TODO make 'hasLocalchanges.size()' thread-save.
+        Set<QueueInternal> theNewInstance = new HashSet<>(max(16, hasLocalChanges.size() / 2));
         mutx.lock();
         try {
-            localCopy = hasLocalChanges;
+            copyToPublish = hasLocalChanges;
+            hasLocalChanges = theNewInstance;
         } finally {
             mutx.unlock();
         }
-        evBus.publish(, );
+        var buf = new ByteArrayOutputStream(64);
+        var writer = new OutputStreamWriter(buf);
+        encode(copyToPublish, writer);
+        try {
+            writer.close();
+        } catch (IOException e) {
+            throw new UncheckedIOException("TODO", e);
+        }
+        evBus.publish(queueUpdatesAddr, buf.toByteArray());
+        vertx.setTimer(publishEveryMs, this::publishLocalChanges);
+    }
+
+    private void onUpdateFromCluster(Message<Object> evBusMsg) {
+        Set<QueueInternal> newQueues = decode((byte[]) evBusMsg.body());
+        mutx.lock();
+        try {
+            /*TODO any way to move this loop outside lock?*/
+            for (QueueInternal newQueue : newQueues) {
+                queues.put(newQueue.name, newQueue);
+            }
+        } finally {
+            mutx.unlock();
+        }
     }
 
     public void resetQueueFailureStatistics(String queueName, BiConsumer<Throwable, Void> onDone) {
@@ -86,8 +122,8 @@ public class QueueStatisticsCollector {
         try {
             assert mutxOwner == null;
             mutxOwner = currentThread();
-            Queue queue = getQueue(queueName);
-            assert queue.name.equals(queueName) : queue.name +", "+ queueName;
+            QueueInternal queue = getQueue(queueName);
+            assert queue.name.equals(queueName) : queue.name + ", " + queueName;
             queue.newestSuccessEpchMs = now;
             hasLocalChanges.add(queue);
         } finally {
@@ -106,7 +142,7 @@ public class QueueStatisticsCollector {
         try {
             assert mutxOwner == null;
             mutxOwner = currentThread();
-            Queue queue = getQueue(queueName);
+            QueueInternal queue = getQueue(queueName);
             queue.newestFailureEpchMs = now;
             hasLocalChanges.add(queue);
         } finally {
@@ -121,7 +157,7 @@ public class QueueStatisticsCollector {
         try {
             assert mutxOwner == null;
             mutxOwner = currentThread();
-            Queue queue = getQueue(queueName);
+            QueueInternal queue = getQueue(queueName);
             assert queue.name.equals(queueName);
             queue.slowdownTimeSec = retryDelaySec;
             hasLocalChanges.add(queue);
@@ -136,7 +172,7 @@ public class QueueStatisticsCollector {
         assert mutxOwner == null;
         mutxOwner = currentThread();
         try {
-            Queue queue = getQueue(queueName);
+            QueueInternal queue = getQueue(queueName);
             queue.backpressureDelayMs = delayReplyMs;
             hasLocalChanges.add(queue);
         } finally {
@@ -145,10 +181,29 @@ public class QueueStatisticsCollector {
         }
     }
 
-    // TODO get rid of ubly 'JsonObject' in return
+    /**
+     * @deprecated use {link #getQueueStatistics2}. WARN: This API bleeds
+     * weakly-typed stuff! The compiler will FAIL to tell you about bugs.
+     */
+    @Deprecated
     public Future<JsonObject> getQueueStatistics(List<String> queueNames) {
-        log.trace("TODO getQueueStatistics({})", queueNames);
-        return Promise.<JsonObject>promise().future(/*TODO*/);
+        return getQueueStatistics2(queueNames).compose(queues -> vertx.executeBlocking(() -> {
+            JsonArray superfluousJsonArray = new JsonArray();
+            for (Queue queue : queues) {
+                JsonObject queueJson = new JsonObject();
+                queueJson.put(MONITOR_QUEUE_NAME, queue.name);
+                /*queueJson.put(TODO, queue.TODO);*/
+                superfluousJsonArray.add(queueJson);
+            }
+            JsonObject superfluousJsonObj = new JsonObject();
+            superfluousJsonObj.put(STATUS, "OK");
+            superfluousJsonObj.put(QUEUES, superfluousJsonArray);
+            return superfluousJsonObj;
+        }));
+    }
+
+    public Future<Set<Queue>> getQueueStatistics2(List<String> queueNames) {
+        throw new UnsupportedOperationException/*TODO*/("not impl yet");
     }
 
     public void getQueuesSpeed(Message<JsonObject> ev, List<String> queueNames) {
@@ -159,23 +214,50 @@ public class QueueStatisticsCollector {
         throw new UnsupportedOperationException/*TODO*/("not impl yet");
     }
 
-    private Queue getQueue(String queueName) {
+    private QueueInternal getQueue(String queueName) {
         assert mutxOwner == currentThread();
-        Queue queue = queues.get(queueName);
-        if(queue == null){
-            queue = new Queue();
+        QueueInternal queue = queues.get(queueName);
+        if (queue == null) {
+            queue = new QueueInternal();
             queue.name = queueName;
             queues.put(queue.name, queue);
         }
         return queue;
     }
 
-    private static class Queue {
-        private String name;
-        private long newestSuccessEpchMs;
-        private long newestFailureEpchMs;
-        private int slowdownTimeSec;
-        private long backpressureDelayMs;
+    private void encode(Set<QueueInternal> queues, Writer dst) {
+        ObjectMapper objectMapper = new ObjectMapper(); /*TODO move*/
+        try {
+            objectMapper.writeValue(dst, queues);
+        } catch (IOException e) {
+            throw new UncheckedIOException("TODO", e);
+        }
+    }
+
+    private Set<QueueInternal> decode(byte[] body) {
+        ObjectMapper objectMapper = new ObjectMapper()/*TODO*/;
+        try {
+            return objectMapper.readValue(body, new TypeReference<Set<QueueInternal>>() {});
+        } catch (IOException e) {
+            throw new UncheckedIOException("TODO", e);
+        }
+    }
+
+
+    private static class QueueInternal {
+        public String name;
+        public long newestSuccessEpchMs = -1;
+        public long newestFailureEpchMs = -1;
+        public int slowdownTimeSec = -1;
+        public long backpressureDelayMs = -1;
+    }
+
+    public static class Queue {
+        public String name;
+        public long newestSuccessEpchMs = -1;
+        public long newestFailureEpchMs = -1;
+        public int slowdownTimeSec = -1;
+        public long backpressureDelayMs = -1;
     }
 
 }
